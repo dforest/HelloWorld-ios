4fa0c48a0ac4e978a4a0e8be03f5ff66696547db
Keita Mori <keita011@gmail.com>
2012-12-18 03:50:33 +0900

    initial commit

---
+++ HelloWorld/HelloWorld/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by Mori Keita on 2012/12/18.
+//  Copyright (c) 2012年 Keita Mori. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloWorld/HelloWorld/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by Mori Keita on 2012/12/18.
+//  Copyright (c) 2012年 Keita Mori. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
